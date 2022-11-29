fa6cbbf4d3be4f473e69feeae4f647c4254e8b42
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2022-11-29 18:12:07 +0100

    Add valid duplicate mail

--- routes/user.ts
+++ routes/user.ts
@@ -11,7 +11,11 @@ userRouter
   })
   .post("/", async (req: Request, res: Response, next: NextFunction) => {
     const { name, email, password } = req.body;
+    const oldUser = await User.findOne({ email });
+    if (oldUser) {
+      return res.status(401).json({ error: "This email is already in use!" });
+    }
     const newUser = new User({ name, email, password });
     await newUser.save();
-    res.json({ user: newUser });
+    res.status(201).json({ user: newUser });
   });
