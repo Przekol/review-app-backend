323d9263bf82ebbb28a86f328337f5cd87bd75e4
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-03 08:20:34 +0100

    Add another validation

--- middlewares/validator.ts
+++ middlewares/validator.ts
@@ -13,6 +13,21 @@ export const userValidator = [
     .withMessage("Password must be 8 to 20 characters long!"),
 ];
 
+export const signInValidator = [
+  body("email").normalizeEmail().isEmail().withMessage("Email is invalid!"),
+  body("password").trim().not().isEmpty().withMessage("Password is missing!"),
+];
+
+export const validateNewPassword = [
+  body("newPassword")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("Password is missing!")
+    .isLength({ min: 8, max: 20 })
+    .withMessage("Password must be 8 to 20 characters long!"),
+];
+
 export const validate = (req: Request, res: Response, next: NextFunction) => {
   const error = validationResult(req).array();
   if (error.length) {
