73d96c943db5a736b916d49a9c71827de6651bbd
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-03 08:18:52 +0100

    Add isValidPassResetToken middleware

---
+++ middlewares/user.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import { PasswordResetToken } from "../records/password-reset-token.record";
+import { sendError } from "../utils/helper";
+import { isValidObjectId } from "mongoose";
+
+export const isValidPassResetToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { token, userId } = req.body;
+
+  if (!token.trim() || !isValidObjectId(userId))
+    return sendError(res, "Invalid request!");
+
+  const resetToken = await PasswordResetToken.findOne({ owner: userId });
+  if (!resetToken)
+    return sendError(res, "Unauthorized access, invalid request!");
+
+  const isMatched = await resetToken.compareToken(token);
+  if (!isMatched)
+    return sendError(res, "Unauthorized access, invalid request!");
+  // @ts-ignore
+  req.resetToken = resetToken;
+  next();
+};
