26c76e9c8ecdb100701f746e31be823904846a0f
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-03 08:19:07 +0100

    Add UserDoc types

---
+++ types/user.ts
@@ -0,0 +1,9 @@
+import { Document } from "mongoose";
+
+export interface UserDoc extends Document {
+  name: string;
+  email: string;
+  password: string;
+  isVerified: boolean;
+  comparePassword: (password: string) => boolean;
+}
